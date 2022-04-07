306e6464c5396ae5284def5ca45df72864884e53
atsushi-okamoto <a.okamoto20@gmail.com>
2022-04-07 17:02:41 +0900

    blogテーブルのマイグレーションファイル

--- database/migrations/2022_04_07_073007_create_blogs_table.php
+++ database/migrations/2022_04_07_073007_create_blogs_table.php
@@ -15,6 +15,8 @@ class CreateBlogsTable extends Migration
     {
         Schema::create('blogs', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->text('body');
             $table->timestamps();
         });
     }
