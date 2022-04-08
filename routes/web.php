5f45ae6da3e2acf7d00cff35bc4af95b117d2513
atsushi-okamoto <a.okamoto20@gmail.com>
2022-04-08 22:20:28 +0900

    ブログ一覧画面のルーティング作成

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// ブログ一覧
+Route::get('/', 'BlogController@showIndex')->name('index');
